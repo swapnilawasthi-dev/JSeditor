c1b2782dbb43a946d78d4b9297f798656394a9f8
swapnilawasthi-dev <165567232+swapnilawasthi-dev@users.noreply.github.com>
2024-04-15 21:52:12 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Home } from './containers';
+
+const App = () => {
+  return (
+    <div className='w-screen h-screen flex items-start justify-start overflow-hidden'>
+      <Routes>
+        <Route path="/home/*" element={<Home />} />
+
+        <Route path='*' element={<Navigate to={"/home"} />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
