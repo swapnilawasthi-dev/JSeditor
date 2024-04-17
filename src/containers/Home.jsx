e9e1383447cabcc673bc2bb04878f81cc67d5c3c
swapnilawasthi-dev <165567232+swapnilawasthi-dev@users.noreply.github.com>
2024-04-17 23:41:38 +0530

    Home Screen top section

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import { HiChevronDoubleLeft } from 'react-icons/hi2';
 import { MdHome } from 'react-icons/md';
+import { FaSearchengin } from 'react-icons/fa6';
 import { motion } from 'framer-motion';
-import { Link } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import logo from '../../public/logo.png'
+import {Projects, SignUp} from '../containers'
 
 const Home = () => {
     const [isSideNav, setIsSideNav] = useState(false);
-    const [user, setUser] = useState(['name']);
+    const [user, setUser] = useState(null);
 
   return (
     <>
@@ -24,12 +26,11 @@ const Home = () => {
                     </motion.div>
 
 
+                <div className='overflow-hidden w-full flex flex-col gap-4'>
                 {/* logo */}
-                    <div className='overflow-hidden w-full flex flex-col gap-4'>
                         <Link to={'/home'}>
                             <img src={logo} className='object-contain w72'  alt='logo'/>
                         </Link>
-                    </div>
 
 
                 {/* open editor */}
@@ -49,11 +50,47 @@ const Home = () => {
                             <p className='text-lg text-primaryText'>Home</p>
                         </Link>
                     )
-                        
+                    
                     
                 }
+                </div>
         
         </div>
+        <div className='flex-1 min-h-screen max-h-screen overflow-y-scroll h-full flex flex-col items-start justify-start px-4 md:px-12 py-4 md:py-12'>
+            {/* top section */}
+            <div className='w-full flex items-center justify-between gap-3'>
+                {/* search */}
+                    <div className='bg-secondary w-full px-4 py-3 rounded-md flex items-center justify-center gap-3'>
+                        <FaSearchengin className='text-2xl text-primaryText' />
+                        <input type='text' className=' flex-1 px-4 py-1 text-xl bg-transparent outline-none border-none text-primaryText placeholder:text-gray-600' placeholder='Search Here....' />
+                    </div>
+                {/* profile section */}
+                    {!user && (
+                        <motion.div whileTap={{scale: 0.9}} className='flex items-center justify-center gap-3'>
+                            <Link to={'/home/auth'} className='bg-emerald-500 px-6 py-2 rounded-md text-white text-lg cursor-pointer hover:bg-emerald-700 '>
+                                Sign Up
+                            </Link>
+                        </motion.div>
+                        )
+                    }
+
+                    {user && (
+                        <div>
+
+                        </div>
+                    )}
+            </div>
+            
+
+            {/* bottom section */}
+            <div className='w-full'>
+                <Routes>
+                    <Route path='/*' element={<Projects />} />
+                    <Route path='/auth' element={<SignUp />} />
+                </Routes>
+            </div>
+
+        </div> 
     </>
   )
 }
---
+++ src/containers/SignUp.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import logo from '../../public/logo.png'
+
+const SignUp = () => {
+  return (
+    <div className='w-full py-6 '>
+        <img 
+            src={logo}
+            className='object-contain w-32 opacity-50 h-auto'
+            alt=""
+        />
+
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default SignUp
