97d99fc98c1723bc11d78c3f1b57f007a23d637b
swapnilawasthi-dev <165567232+swapnilawasthi-dev@users.noreply.github.com>
2024-05-24 20:56:31 +0530

    Login complete

--- src/components/UserAuthInput.jsx
+++ src/components/UserAuthInput.jsx
@@ -1,52 +1,68 @@
-import React, { useState } from 'react'
-import { FaEnvelope, FaEye, FaEyeSlash } from 'react-icons/fa6'
-import { motion } from 'framer-motion'
+import React, { useState } from "react";
+import { FaEnvelope, FaEye, FaEyeSlash } from "react-icons/fa6";
+import { motion } from "framer-motion";
 
-const UserAuthInput = ({label, placeHolder, isPass, key, setStateFunction, Icon, setGetEmailValidationStatus}) => {
-    const [value, setValue] = useState("")
-    const [showPass, setShowPass] = useState(true)
-    const [isValid, setIsValid] = useState(false)
+const UserAuthInput = ({
+  label,
+  placeHolder,
+  isPass,
+  setStateFunction,
+  Icon,
+  setGetEmailValidationStatus,
+}) => {
+  const [value, setValue] = useState("");
+  const [showPass, setShowPass] = useState(true);
+  const [isValid, setIsValid] = useState(false);
 
-    const handleTextChange = (e) => {
-        setValue(e.target.value);
-        setStateFunction(e.target.value);
+  const handleTextChange = (e) => {
+    setValue(e.target.value);
+    setStateFunction(e.target.value);
 
-        if(label === "Email"){
-            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/ ;
-            const status = emailRegex.test(value);
-            setIsValid(status);
-            setGetEmailValidationStatus(status);
-        }
+    if (label === "Email") {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      const status = emailRegex.test(value);
+      setIsValid(status);
+      setGetEmailValidationStatus(status);
     }
+  };
 
   return (
-    <div className=' flex flex-col items-start justify-start gap-1'>
-        <label className=' text-sm text-gray-300'>{label}</label>
-        <div className={`flex items-center justify-center gap-3 w-full md:w-96 rounded-md px-4 py-1 bg-gray-200
-            ${!isValid && label === "Email" && value.length > 0 && "border-2 border-red-500"}`}>
-            <Icon className=' text-text555 text-2xl' />
-            <input
-                type={isPass && showPass ? "password" :"text"}
-                placeholder={placeHolder}
-                className=' flex-1 w-full h-full py-2 outline-none border-none bg-transparent text-text555 text-lg'
-                value={value}
-                onChange={handleTextChange}
-            />
+    <div className=" flex flex-col items-start justify-start gap-1">
+      <label className=" text-sm text-gray-300">{label}</label>
+      <div
+        className={`flex items-center justify-center gap-3 w-full md:w-96 rounded-md px-4 py-1 bg-gray-200
+            ${
+              !isValid &&
+              label === "Email" &&
+              value.length > 0 &&
+              "border-2 border-red-500"
+            }`}
+      >
+        <Icon className=" text-text555 text-2xl" />
+        <input
+          type={isPass && showPass ? "password" : "text"}
+          placeholder={placeHolder}
+          className=" flex-1 w-full h-full py-2 outline-none border-none bg-transparent text-text555 text-lg"
+          value={value}
+          onChange={handleTextChange}
+        />
 
-            {isPass && (
-                <motion.div onClick={() => setShowPass(!showPass)} whileTap={{scale: 0.95}} className='cursor-pointer'>
-                    { showPass ? (
-                            <FaEye className=' text-text555 text-2xl' />
-                        ) : (
-                            <FaEyeSlash className=' text-text555 text-2xl' />
-                        )
-                        
-                    }
-                </motion.div> 
-            )}   
-        </div>
+        {isPass && (
+          <motion.div
+            onClick={() => setShowPass(!showPass)}
+            whileTap={{ scale: 0.95 }}
+            className="cursor-pointer"
+          >
+            {showPass ? (
+              <FaEye className=" text-text555 text-2xl" />
+            ) : (
+              <FaEyeSlash className=" text-text555 text-2xl" />
+            )}
+          </motion.div>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default UserAuthInput
+export default UserAuthInput;
--- src/components/UserProfileDetails.jsx
+++ src/components/UserProfileDetails.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { FaChevronDown } from "react-icons/fa6";
+import { Menus, signOutAction } from "../utils/helpers";
+import { Link } from "react-router-dom";
+import { slideUpOut } from "../animations";
 
 const UserProfileDetails = () => {
   const user = useSelector((state) => state.user?.user);
+  const [isMenu, setIsMenu] = useState(false);
   return (
     <div className="flex items-center justify-center gap-4 relative ">
       <div className=" w-14 h-14 flex items-center justify-center rounded-xl overflow-hidden cursor-pointer bg-emerald-500">
@@ -25,13 +29,39 @@ const UserProfileDetails = () => {
         )}
       </div>
 
-      <motion.div whileTap={{scale: 0.9}} className=" p-4 rounded-md flex items-center justify-center bg-secondary cursor-pointer">
+      <motion.div
+        onClick={() => setIsMenu(!isMenu)}
+        whileTap={{ scale: 0.9 }}
+        className=" p-4 rounded-md flex items-center justify-center bg-secondary cursor-pointer"
+      >
         <FaChevronDown className=" text-primaryText" />
       </motion.div>
-
-      <motion.div className=" bg-secondary absolute top-16 right-0 px-4 py-3 rounded-xl shadow-md z-10 flex flex-col items-start justify-start gap-4 min-w-[225px] ">
-
-      </motion.div>
+      <AnimatePresence>
+        {isMenu && (
+          <motion.div
+            {...slideUpOut}
+            className=" bg-secondary absolute top-16 right-0 px-4 py-3 rounded-xl shadow-md z-10 flex flex-col items-start justify-start gap-4 min-w-[225px] "
+          >
+            {Menus &&
+              Menus.map((menu) => (
+                <Link
+                  to={menu.uri}
+                  key={menu.id}
+                  className=" text-primaryText text-lg hover:bg-[rgba(256,256,256,0.5)] hover:text-black px-2 py-1 w-full rounded-md"
+                >
+                  {menu.name}
+                </Link>
+              ))}
+            <motion.p
+              onClick={signOutAction}
+              whileTap={{ scale: 0.9 }}
+              className=" text-primaryText text-lg hover:bg-[rgba(256,256,256,0.5)] hover:text-black px-2 py-1 w-full rounded-md cursor-pointer"
+            >
+              Sign Out
+            </motion.p>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
